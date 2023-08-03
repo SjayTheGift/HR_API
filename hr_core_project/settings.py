5b355380fdced55709aed1c71ac9ebd7d03a0332
Esethu <jonas.esethu24@gmail.com>
2023-08-03 03:30:23 +0200

    add database

--- hr_core_project/settings.py
+++ hr_core_project/settings.py
@@ -109,10 +109,22 @@ WSGI_APPLICATION = 'hr_core_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('PRD_DB_USER'),
+        'PASSWORD': env('PRD_DB_PASSWORD'),
+        'HOST': env('PRD_HOST'),
+        'PORT': '5432',
     }
 }
 
