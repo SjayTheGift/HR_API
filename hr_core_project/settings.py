56785f85167829573c92c635bf5b83bed1dc3c35
Esethu <jonas.esethu24@gmail.com>
2023-08-07 23:22:46 +0200

    uncomment database

--- hr_core_project/settings.py
+++ hr_core_project/settings.py
@@ -109,24 +109,24 @@ WSGI_APPLICATION = 'hr_core_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env('DB_NAME'),
-#         'USER': env('PRD_DB_USER'),
-#         'PASSWORD': env('PRD_DB_PASSWORD'),
-#         'HOST': env('PRD_HOST'),
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('PRD_DB_USER'),
+        'PASSWORD': env('PRD_DB_PASSWORD'),
+        'HOST': env('PRD_HOST'),
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
